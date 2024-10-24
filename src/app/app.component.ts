a62dbd1e4a1dff510d37a081a75a500d50a0045f
Kaushik Dange <kaushikdange3107@gmail.com>
2024-10-24 17:27:06 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'codepen';
+  htmlCode: string = '<h1>Hello World</h1>';
+  cssCode: string = 'h1 { color: red; }';
+  jsCode: string = 'console.log("Hello World");';
+
+  @ViewChild('previewFrame', { static: false })
+  previewFrame!: ElementRef;
+
+  ngAfterViewChecked() {
+    this.updatePreview();
+  }
+
+  updatePreview() {
+    const document = this.previewFrame.nativeElement.contentDocument;
+    const content = `
+      <html>
+        <head>
+          <style>${this.cssCode}</style>
+        </head>
+        <body>
+          ${this.htmlCode}
+          <script>${this.jsCode}<\/script>
+        </body>
+      </html>
+    `;
+    document.open();
+    document.write(content);
+    document.close();
+  }
 }
